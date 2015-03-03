a6258fd469b049bf03b49be650c88ab5796cbe26
Alexander Ewetumo <trinoxf@gmail.com>
2015-03-03 23:07:53 +0100

    new route

--- web.grid.go
+++ web.grid.go
@@ -7,9 +7,14 @@
 package webgrid
 
 import (
+	"fmt"
 	"github.com/influx6/fsgrid"
 	"github.com/influx6/grids"
+	"github.com/influx6/reggy"
+	"mime"
 	"net/http"
+	fpath "path/filepath"
+	"strings"
 	// "encoding/json"
 	// "errors"
 	// "io"
@@ -24,6 +29,8 @@ import (
 	// "unicode/utf8"
 )
 
+type ActionMethod func(res http.ResponseWriter, req *http.Request, p *grids.GridPacket)
+
 type HttpServ struct {
 	*grids.Grid
 }
@@ -37,14 +44,50 @@ type StaticServ struct {
 	// ErrorRender *Render
 }
 
-type MethodFilter struct {
-	method string
+type View struct {
 	*grids.Grid
+	Render *Render
+}
+
+type FileView struct {
+	*View
+	File *fsgrid.FileReader
 }
 
 type Render struct {
 	Handler func(i interface{}, r interface{})
 	*grids.Grid
+}
+
+type MethodFilter struct {
+	method string
+	*grids.Grid
+}
+
+type Reply struct {
+	RequestCallback ActionMethod
+	*grids.Grid
+}
+
+type Route struct {
+	*grids.Grid
+	Route *reggy.ClassicMatchMux
+}
+
+func UseHttpPacket(resq *grids.GridPacket, caller func(rw http.ResponseWriter, r *http.Request)) {
+	req, ok := resq.Body["req"].(*http.Request)
+
+	if !ok {
+		return
+	}
+
+	res, ek := resq.Body["res"].(http.ResponseWriter)
+
+	if !ek {
+		return
+	}
+
+	caller(res, req)
 }
 
 func NewRender(format string, m func(i interface{}, f interface{})) *Render {
@@ -89,8 +132,15 @@ func FileRender() *Render {
 			return
 		}
 
-		// uri := req.URL.Path
-		var _ = req
+		var ctype string
+
+		uri := req.URL.Path
+
+		if file, ok := resq.Body["file"].(string); !ok {
+			ctype = mime.TypeByExtension(fpath.Ext(uri))
+		} else {
+			ctype = mime.TypeByExtension(fpath.Ext(file))
+		}
 
 		data := resq.Obj().([]interface{})
 		bu := make([]byte, len(data))
@@ -99,9 +149,27 @@ func FileRender() *Render {
 			bu[k] = v.(byte)
 		}
 
+		res.Header().Set("Content-Type", ctype)
 		res.Write(bu)
 	})
 	return r
+}
+
+func NewRenderView() *View {
+	v := &View{grids.NewGrid("FileView"), FileRender()}
+	v.NewIn("req")
+	v.NewOut("err")
+	return v
+}
+
+func NewFileView(file string) *FileView {
+	v := &FileView{NewRenderView(), fsgrid.ReadFile(file)}
+
+	v.InBind("req", v.File.In("file"))
+	v.File.OutBind("res", v.Render.In("req"))
+	v.File.OutBind("err", v.Out("err"))
+
+	return v
 }
 
 func ErrorRender(response func(rw http.ResponseWriter, r *http.Request)) *Render {
@@ -137,12 +205,66 @@ func ErrorRender(response func(rw http.ResponseWriter, r *http.Request)) *Render
 	return r
 }
 
+func StripPath(stripBase string) *grids.Grid {
+	r := grids.NewGrid("web.StripPath")
+	r.NewIn("req")
+	r.NewOut("res")
+
+	ssb := strings.TrimPrefix(stripBase, "/")
+	reqs := r.In("req")
+
+	reqs.End(func(packet interface{}, next func(i interface{})) {
+		resq, err := packet.(*grids.GridPacket)
+		if !err {
+			return
+		}
+
+		req, ok := resq.Body["req"].(*http.Request)
+
+		if !ok {
+			return
+		}
+
+		_, ek := resq.Body["res"].(http.ResponseWriter)
+
+		if !ek {
+			return
+		}
+
+		path := strings.TrimPrefix(fpath.Clean(req.URL.Path), "/")
+		hs := strings.HasPrefix(path, stripBase)
+		if hs {
+			path = strings.Replace(path, stripBase, "", 1)
+		}
+
+		hs = strings.HasPrefix(path, ssb)
+
+		if hs {
+			path = strings.Replace(path, ssb, "", 1)
+		}
+
+		path = fpath.Clean(path)
+
+		resq.Body["file"] = path
+
+		r.OutSend("res", resq)
+
+		next(nil)
+	})
+
+	return r
+}
+
 func NewHttp() *HttpServ {
 	sv := &HttpServ{grids.NewGrid("ServIO")}
+	sv.NewIn("req")
+	sv.NewOut("res")
+	sv.NewOut("err")
+	sv.InBind("req", sv.Out("res"))
 	return sv
 }
 
-func NewStatic(base string) *StaticServ {
+func NewStaticBase(base string, stripbase string) *StaticServ {
 	control, err := fsgrid.CreateFSControl(base)
 
 	if err != nil {
@@ -150,13 +272,13 @@ func NewStatic(base string) *StaticServ {
 	}
 
 	reader := fsgrid.CreateFSFile()
+	stripper := StripPath(stripbase)
 
 	st := &StaticServ{grids.NewGrid("web.StaticServer"), base, control, reader}
 	st.NewIn("req")
 	st.NewOut("res")
 	st.NewOut("err")
 
-	reqs := st.In("req")
 	rd := reader.Out("res")
 
 	rd.Or(func(i interface{}) {
@@ -165,6 +287,7 @@ func NewStatic(base string) *StaticServ {
 		if !err {
 			return
 		}
+
 		st.OutSend("res", resq)
 	})
 
@@ -173,6 +296,7 @@ func NewStatic(base string) *StaticServ {
 		if !err {
 			return
 		}
+
 		st.OutSend("err", resq)
 	}
 
@@ -189,39 +313,31 @@ func NewStatic(base string) *StaticServ {
 			return
 		}
 
-		reader.InSend("file", resq)
+		reader.InSend("read", resq)
 	})
 
-	reqs.End(func(packet interface{}, next func(i interface{})) {
+	st.InBind("req", stripper.In("req"))
+
+	stripper.Out("res").End(func(packet interface{}, next func(i interface{})) {
 		resq, err := packet.(*grids.GridPacket)
 
 		if !err {
 			return
 		}
 
-		req, ok := resq.Body["req"].(*http.Request)
+		path, ok := resq.Body["file"]
 
 		if !ok {
 			return
 		}
 
-		res, ek := resq.Body["res"].(http.ResponseWriter)
-
-		if !ek {
-			return
-		}
-
-		path := req.URL.Path
-
 		if path == "/" || path == "./" {
 			st.OutSend("err", resq)
+			next(nil)
 			return
 		}
 
-		pkmap := grids.GridMap{"res": res, "req": req, "file": path}
-		pk := grids.CreateGridPacket(pkmap)
-
-		control.InSend("file", pk)
+		control.InSend("file", resq)
 
 		next(nil)
 	})
@@ -229,8 +345,16 @@ func NewStatic(base string) *StaticServ {
 	return st
 }
 
-func NewStaticServo(base string) *StaticServ {
-	st := NewStatic(base)
+func NewStatic(base string, strip string) *StaticServ {
+	m := NewStaticBase(base, strip)
+	r := FileRender()
+
+	m.OutBind("res", r.In("req"))
+	return m
+}
+
+func NewStaticServo(base string, strip string) *StaticServ {
+	st := NewStaticBase(base, strip)
 
 	startTemp := `
 		<!doctype>
@@ -267,7 +391,7 @@ func NewStaticServo(base string) *StaticServ {
 			<body>
 	`
 
-	endTemp := `	
+	endTemp := `
 			</body>
 		</html>
 	`
@@ -311,5 +435,98 @@ func (s *StaticServ) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
 func (h *HttpServ) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	vals := map[interface{}]interface{}{"res": rw, "req": r}
-	h.OutSend("req", grids.CreateGridPacket(vals))
+	h.InSend("req", grids.CreateGridPacket(vals))
+}
+
+func NewMethodFilter(method string) *MethodFilter {
+	m := &MethodFilter{method, grids.NewGrid("MethodFilter")}
+
+	m.NewIn("req")
+	m.NewOut("res")
+	m.NewOut("rej")
+
+	m.AndIn("req", func(p *grids.GridPacket, next func(f interface{})) {
+
+		UseHttpPacket(p, func(res http.ResponseWriter, req *http.Request) {
+			med := strings.ToLower(req.Method)
+
+			if method == med {
+				m.OutSend("res", p)
+				return
+			}
+			m.OutSend("res", p)
+			next(p)
+			return
+		})
+
+	})
+
+	return m
+}
+
+func NewReply(action ActionMethod) *Reply {
+	m := &Reply{action, grids.NewGrid("Reply.Action")}
+	m.NewIn("req")
+
+	m.AndIn("req", func(p *grids.GridPacket, next func(f interface{})) {
+		UseHttpPacket(p, func(res http.ResponseWriter, req *http.Request) {
+			action(res, req, p)
+			next(p)
+		})
+	})
+
+	return m
+}
+
+func NewRoute(pattern string, strict bool) *Route {
+	m := &Route{grids.NewGrid("Route.Action"), reggy.CreateClassic(pattern)}
+	m.NewIn("req")
+
+	m.NewOut("no")
+	m.NewOut("yes")
+
+	m.AndIn("req", func(p *grids.GridPacket, next func(f interface{})) {
+		UseHttpPacket(p, func(res http.ResponseWriter, req *http.Request) {
+			path := req.URL.Path
+
+			passed, params := m.Route.Validate(path, strict)
+
+			if !passed {
+				m.OutSend("no", p)
+				return
+			}
+
+			p.Body["params"] = params
+			m.OutSend("yes", p)
+			next(p)
+		})
+	})
+
+	return m
+}
+
+func NewConsoler() *grids.Grid {
+	g := grids.NewGrid("web.Consoler")
+	g.NewIn("in")
+
+	g.OrIn("in", func(g *grids.GridPacket) {
+		UseHttpPacket(g, func(_ http.ResponseWriter, req *http.Request) {
+			fmt.Println("New Request:", req.URL.Path, "Method:", req.Method)
+		})
+	})
+
+	return g
+}
+
+func NewHttpConsole(s *HttpServ) *grids.Grid {
+	c := NewConsoler()
+	s.InBind("req", c.In("in"))
+	return c
+}
+
+func NewStaticConsole(s *StaticServ) *grids.Grid {
+	c := NewConsoler()
+	s.InBind("req", c.In("in"))
+	s.InBind("err", c.In("in"))
+	return c
 }
